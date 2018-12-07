ae34f7c89886c4b064272a0c73822cbe210aa567
Wong Hoi Sing Edison <hswong3i@gmail.com>
2018-12-07 14:31:38 +0800

    move to ansible based deployment

---
+++ files/usr/local/bin/docker-entrypoint.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+set -o xtrace
+
+# Ensure required folders exist with correct owner:group
+mkdir -p $CONFLUENCE_HOME
+chown -Rf $CONFLUENCE_OWNER:$CONFLUENCE_GROUP $CONFLUENCE_HOME
+chmod 0755 $CONFLUENCE_HOME
+
+# Running Confluence in foreground
+exec /etc/init.d/confluence start -fg
